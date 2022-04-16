fccf166e101a659e5e13a6f013a328ce069f451a
karolina-eledzhikova <103732237+karolina-eledzhikova@users.noreply.github.com>
2022-04-16 20:55:55 +0300

    Add files via upload

---
+++ Objects and Classes/task_demo.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age_name = age
+
+
+ivan = Person("Ivan", "Ivanov", 23)
+print(ivan.first_name)
